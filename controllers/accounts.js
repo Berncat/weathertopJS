7b1bb5942a56d3fd13eca0f258bd63677f408b8d
Glitch (weather-top-2021) <none>
2021-09-13 00:29:53 +0000

    🐣👆 Updated with Glitch

---
+++ controllers/accounts.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const logger = require("../utils/logger");
+const userStore = require("../models/user-store");
+const uuid = require("uuid");
+
+const accounts = {
+  index(request, response) {
+    logger.info("Home rendering");
+    const viewData = {
+      title: "Home"
+    };
+    response.render("index", viewData);
+  },
+
+  login(request, response) {
+    logger.info("Login rendering");
+    const viewData = {
+      title: "Login"
+    };
+    response.render("login", viewData);
+  },
+
+  logout(request, response) {
+    logger.info("Logging out " + accounts.getCurrentUser(request).email);
+    response.cookie("station", "");
+    response.redirect("/");
+  },
+
+  signup(request, response) {
+    logger.info("Sign up rendering");
+    const viewData = {
+      title: "Sign up"
+    };
+    response.render("signup", viewData);
+  },
+
+  edit(request, response) {
+    logger.info("Edit details rendering");
+    const viewData = {
+      title: "Edit details"
+    };
+    response.render("edit", viewData);
+  },
+
+  update(request, response) {
+    const user = accounts.getCurrentUser(request);
+    logger.info(user);
+    const updatedUser = request.body;
+    logger.info(updatedUser);
+    userStore.updateUser(user, updatedUser);
+    response.cookie("station", "");
+    response.redirect("/login");
+  },
+
+  register(request, response) {
+    const user = request.body;
+    let checkEmail = true;
+    const users = userStore.getAllUsers()
+
+    for (let i = 0; i < users.length; i++) {
+      if (request.body.email === users[i].email) {
+        checkEmail = false;
+      }
+    }
+
+    if (checkEmail) {
+      user.id = uuid.v1();
+      userStore.addUser(user);
+      logger.info(`Registering ${user.email}`);
+      response.redirect("/login");
+    } else {
+      response.redirect("/signup");
+    }
+  },
+
+  authenticate(request, response) {
+    const user = userStore.getUserByEmail(request.body.email);
+    if ((user) && (user.password === request.body.password)) {
+      response.cookie("station", user.email);
+      logger.info(`Logging in ${user.email}`);
+      response.redirect("/dashboard");
+    } else {
+      response.redirect("/login");
+    }
+  },
+
+  getCurrentUser(request) {
+    const userEmail = request.cookies.station;
+    return userStore.getUserByEmail(userEmail);
+  }
+};
+
+module.exports = accounts;
---
+++ controllers/dashboard.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const accounts = require("./accounts.js");
+const logger = require("../utils/logger");
+const stationStore = require("../models/station-store");
+const uuid = require("uuid");
+
+const dashboard = {
+  index(request, response) {
+
+    try {
+      logger.info("Rendering Dashboard");
+      const loggedInUser = accounts.getCurrentUser(request);
+      const viewData = {
+        title: "Dashboard",
+        stations: stationStore.getUserStations(loggedInUser.id)
+      };
+      response.render("dashboard", viewData);
+    } catch (err) {
+      response.redirect("/login");
+    }
+  },
+
+  deleteStation(request, response) {
+    const stationId = request.params.id;
+    logger.info("Deleting Station: " + stationStore.getStation(stationId).name);
+    stationStore.removeStation(stationId);
+    response.redirect("/dashboard");
+  },
+
+  addStation(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    const newStation = {
+      id: uuid.v1(),
+      userid: loggedInUser.id,
+      name: request.body.name,
+      latitude: request.body.latitude,
+      longitude: request.body.longitude,
+      report: {},
+      readings: []
+    };
+    logger.info("Adding Station: " + newStation.name);
+    stationStore.addStation(newStation);
+    response.redirect("/dashboard");
+  }
+};
+
+module.exports = dashboard;
